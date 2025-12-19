cb754ecf4e5a3f8e7ad4ddac14059eb468b344e3
Technomancer <Da-Technomancer@users.noreply.github.com>
2025-12-18 18:17:20 -0800

    Fix double-rendering the background of UIs

--- src/main/java/com/Da_Technomancer/essentials/api/BlockMenuScreen.java
+++ src/main/java/com/Da_Technomancer/essentials/api/BlockMenuScreen.java
@@ -32,7 +32,7 @@ public abstract class BlockMenuScreen<T extends BlockMenuContainer<U>, U extends
 
 	@Override
 	public void render(GuiGraphics matrix, int mouseX, int mouseY, float partialTicks){
-		renderBackground(matrix, mouseX, mouseY, partialTicks);
+//		renderBackground(matrix, mouseX, mouseY, partialTicks);
 		super.render(matrix, mouseX, mouseY, partialTicks);
 		renderTooltip(matrix, mouseX, mouseY);
 		if(getSlotUnderMouse() == null){
